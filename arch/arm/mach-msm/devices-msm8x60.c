83b98940b2095d49b4e46e31208ec33b86cf03b2
carvsdriver <carvsdriver@gmail.com>
2013-06-11 23:21:08 -0400

    gpu: add 228MHz & 266MHz 2D gpu frequencies

--- arch/arm/mach-msm/devices-msm8x60.c
+++ arch/arm/mach-msm/devices-msm8x60.c
@@ -676,6 +676,26 @@ static struct msm_bus_vectors grp2d0_init_vectors[] = {
 	},
 };
 
+#ifdef CONFIG_KGSL_GPU_CTRL
+static struct msm_bus_vectors grp2d0_low_vectors[] = {
+	{
+		.src = MSM_BUS_MASTER_GRAPHICS_2D_CORE0,
+                .dst = MSM_BUS_SLAVE_EBI_CH0,
+                .ab = 0,
+		.ib = KGSL_CONVERT_TO_MBPS(1300),
+	},
+};
+
+static struct msm_bus_vectors grp2d0_nom_vectors[] = {
+	{
+                .src = MSM_BUS_MASTER_GRAPHICS_2D_CORE0,
+                .dst = MSM_BUS_SLAVE_EBI_CH0,
+                .ab = 0,
+                .ib = KGSL_CONVERT_TO_MBPS(1638),
+        },
+};
+#endif
+
 static struct msm_bus_vectors grp2d0_max_vectors[] = {
 	{
 		.src = MSM_BUS_MASTER_GRAPHICS_2D_CORE0,
@@ -694,6 +714,16 @@ static struct msm_bus_paths grp2d0_bus_scale_usecases[] = {
 		ARRAY_SIZE(grp2d0_init_vectors),
 		grp2d0_init_vectors,
 	},
+#ifdef CONFIG_KGSL_GPU_CTRL
+	{
+		ARRAY_SIZE(grp2d0_low_vectors),
+		grp2d0_low_vectors,
+	},
+	{
+		ARRAY_SIZE(grp2d0_nom_vectors),
+		grp2d0_nom_vectors,
+	},
+#endif
 	{
 		ARRAY_SIZE(grp2d0_max_vectors),
 		grp2d0_max_vectors,
@@ -715,6 +745,26 @@ static struct msm_bus_vectors grp2d1_init_vectors[] = {
 	},
 };
 
+#ifdef CONFIG_KGSL_GPU_CTRL
+static struct msm_bus_vectors grp2d1_low_vectors[] = {
+        {
+                .src = MSM_BUS_MASTER_GRAPHICS_2D_CORE1,
+                .dst = MSM_BUS_SLAVE_EBI_CH0,
+                .ab = 0,
+                .ib = KGSL_CONVERT_TO_MBPS(1300),
+        },
+};
+
+static struct msm_bus_vectors grp2d1_nom_vectors[] = {
+        {
+                .src = MSM_BUS_MASTER_GRAPHICS_2D_CORE1,
+                .dst = MSM_BUS_SLAVE_EBI_CH0,
+                .ab = 0,
+                .ib = KGSL_CONVERT_TO_MBPS(1638),
+        },
+};
+#endif
+
 static struct msm_bus_vectors grp2d1_max_vectors[] = {
         {
                 .src = MSM_BUS_MASTER_GRAPHICS_2D_CORE1,
@@ -733,6 +783,16 @@ static struct msm_bus_paths grp2d1_bus_scale_usecases[] = {
 		ARRAY_SIZE(grp2d1_init_vectors),
 		grp2d1_init_vectors,
 	},
+#ifdef CONFIG_KGSL_GPU_CTRL
+        {
+                ARRAY_SIZE(grp2d1_low_vectors),
+                grp2d1_low_vectors,
+        },
+        {
+                ARRAY_SIZE(grp2d1_nom_vectors),
+                grp2d1_nom_vectors,
+        },
+#endif
 	{
 		ARRAY_SIZE(grp2d1_max_vectors),
 		grp2d1_max_vectors,
@@ -858,6 +918,16 @@ static struct resource kgsl_2d0_resources[] = {
 
 static struct kgsl_device_platform_data kgsl_2d0_pdata = {
 	.pwrlevel = {
+#ifdef CONFIG_KGSL_GPU_CTRL
+		{
+			.gpu_freq = 266667000,
+			.bus_freq = 3,
+		},
+		{
+			.gpu_freq = 228571000,
+			.bus_freq = 2,
+		},
+#endif
 		{
 			.gpu_freq = 200000000,
 			.bus_freq = 1,
@@ -867,8 +937,13 @@ static struct kgsl_device_platform_data kgsl_2d0_pdata = {
 			.bus_freq = 0,
 		},
 	},
+#ifdef CONFIG_KGSL_GPU_CTRL
+	.init_level = 3,
+	.num_levels = 4,
+#else
 	.init_level = 0,
 	.num_levels = 2,
+#endif
 	.set_grp_async = NULL,
 	.idle_timeout = HZ/10,
 	.nap_allowed = true,
@@ -905,17 +980,32 @@ static struct resource kgsl_2d1_resources[] = {
 
 static struct kgsl_device_platform_data kgsl_2d1_pdata = {
 	.pwrlevel = {
-		{
-			.gpu_freq = 200000000,
-			.bus_freq = 1,
-		},
-		{
-			.gpu_freq = 200000000,
-			.bus_freq = 0,
-		},
+#ifdef CONFIG_KGSL_GPU_CTRL
+                {
+                        .gpu_freq = 266667000,
+                        .bus_freq = 3,
+                },
+                {
+                        .gpu_freq = 228571000,
+                        .bus_freq = 2,
+                },
+#endif
+                {
+                        .gpu_freq = 200000000,
+                        .bus_freq = 1,
+                },
+                {
+                        .gpu_freq = 200000000,
+                        .bus_freq = 0,
+                },
 	},
+#ifdef CONFIG_KGSL_GPU_CTRL
+	.init_level = 3,
+	.num_levels = 4,
+#else
 	.init_level = 0,
 	.num_levels = 2,
+#endif
 	.set_grp_async = NULL,
 	.idle_timeout = HZ/10,
 	.nap_allowed = true,
