c74765dca1b9fb949261ba3451ee031ec8c3759e
Chester Kener <Cl3Kener@gmail.com>
2013-10-18 13:48:52 -0400

    Update sync.c
    This file had fallen far behind other popular kernels. This is a squashed cherry-pick of Erik Gilling on this file.  There were occasional contributions from others but this was mostly all of his work.

--- drivers/base/sync.c
+++ drivers/base/sync.c
@@ -16,7 +16,6 @@
 
 #include <linux/debugfs.h>
 #include <linux/export.h>
-#include <linux/module.h>
 #include <linux/file.h>
 #include <linux/fs.h>
 #include <linux/kernel.h>
@@ -35,7 +34,7 @@
 static void sync_fence_signal_pt(struct sync_pt *pt);
 static int _sync_pt_has_signaled(struct sync_pt *pt);
 static void sync_fence_free(struct kref *kref);
-static void sync_dump(struct sync_fence *fence);
+static void sync_dump(void);
 
 static LIST_HEAD(sync_timeline_list_head);
 static DEFINE_SPINLOCK(sync_timeline_list_lock);
@@ -612,16 +611,14 @@ int sync_fence_wait(struct sync_fence *fence, long timeout)
 
 	if (fence->status < 0) {
 		pr_info("fence error %d on [%p]\n", fence->status, fence);
-		sync_dump(fence);
+		sync_dump();
 		return fence->status;
 	}
 
-	if (fence->status == 0) {
-		if (timeout > 0) {
-			pr_info("fence timeout on [%p] after %dms\n", fence,
-				jiffies_to_msecs(timeout));
-			sync_dump(fence);
-		}
+	if (fence->status == 0 && timeout > 0) {
+		pr_info("fence timeout on [%p] after %dms\n", fence,
+			jiffies_to_msecs(timeout));
+		sync_dump();
 		return -ETIME;
 	}
 
@@ -743,7 +740,7 @@ err_put_fd:
 	return err;
 }
 
-static int sync_fill_pt_info(struct sync_pt *pt, void *data, int size)
+int sync_fill_pt_info(struct sync_pt *pt, void *data, int size)
 {
 	struct sync_pt_info *info = data;
 	int ret;
@@ -770,6 +767,7 @@ static int sync_fill_pt_info(struct sync_pt *pt, void *data, int size)
 
 	return info->len;
 }
+
 
 static long sync_fence_ioctl_fence_info(struct sync_fence *fence,
 					unsigned long arg)
@@ -987,7 +985,7 @@ late_initcall(sync_debugfs_init);
 
 #define DUMP_CHUNK 256
 static char sync_dump_buf[64 * 1024];
-static void sync_dump(struct sync_fence *fence)
+void sync_dump(void)
 {
        struct seq_file s = {
                .buf = sync_dump_buf,
@@ -995,9 +993,7 @@ static void sync_dump(struct sync_fence *fence)
        };
        int i;
 
-       seq_printf(&s, "fence:\n--------------\n");
-       sync_print_fence(&s, fence);
-       seq_printf(&s, "\n");
+       sync_debugfs_show(&s, NULL);
 
        for (i = 0; i < s.count; i += DUMP_CHUNK) {
                if ((s.count - i) > DUMP_CHUNK) {
@@ -1012,7 +1008,7 @@ static void sync_dump(struct sync_fence *fence)
        }
 }
 #else
-static void sync_dump(struct sync_fence *fence)
+static void sync_dump(void)
 {
 }
 #endif
