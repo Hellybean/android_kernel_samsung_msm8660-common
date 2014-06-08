4f879cbaecf384fdd1e1a4563fbf2021c4291ce3
Sultanxda <sultanxda@gmail.com>
2014-06-08 11:34:00 -0700

    Revert "bluetooth: Fix race conditions in BT LPM"
    This reverts commit 856da7272ab85b22bdc9940eba1d15e24b1af3b5.

--- drivers/tty/serial/msm_serial_hs.c
+++ drivers/tty/serial/msm_serial_hs.c
@@ -1707,7 +1707,7 @@ static int msm_hs_startup(struct uart_port *uport)
 	if (ret)
 		dev_err(uport->dev, "set active error:%d\n", ret);
 	pm_runtime_enable(uport->dev);
-	pm_runtime_get_sync(uport->dev);
+
 
 	return 0;
 }
@@ -2000,14 +2000,10 @@ static void msm_hs_shutdown(struct uart_port *uport)
 	tasklet_kill(&msm_uport->rx.tlet);
 	cancel_delayed_work_sync(&msm_uport->rx.flip_insert_work);
 
-	/* Free the interrupt */
-	free_irq(uport->irq, msm_uport);
-
 	clk_enable(msm_uport->clk);
 
 	pm_runtime_disable(uport->dev);
 	pm_runtime_set_suspended(uport->dev);
-	pm_runtime_put_sync(uport->dev);
 
 	spin_lock_irqsave(&uport->lock, flags);
 	/* Disable the transmitter */
@@ -2040,6 +2036,8 @@ static void msm_hs_shutdown(struct uart_port *uport)
 	if (use_low_power_wakeup(msm_uport))
 		irq_set_irq_wake(msm_uport->wakeup.irq, 0);
 
+	/* Free the interrupt */
+	free_irq(uport->irq, msm_uport);
 	if (use_low_power_wakeup(msm_uport))
 		free_irq(msm_uport->wakeup.irq, msm_uport);
 }
