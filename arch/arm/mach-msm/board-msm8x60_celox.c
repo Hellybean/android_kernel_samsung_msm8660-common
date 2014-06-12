30b8678c8aa0a75c759e28f4b55311cf5d1c8357
Emmanuel Utomi <emmanuelutomi@gmail.com>
2014-06-12 18:16:57 -0500

    msm8x60_celox: Fixup Memory map
    825mb of userspace (RAM)
    Thanks to @sultanxda for a reference.

--- arch/arm/mach-msm/board-msm8x60_celox.c
+++ arch/arm/mach-msm/board-msm8x60_celox.c
@@ -136,7 +136,6 @@
 #include <mach/restart.h>
 #include <mach/board-msm8660.h>
 #include <mach/devices-lte.h>
-#include <mach/iommu_domains.h>
 #ifdef CONFIG_ANDROID_RAM_CONSOLE
 #include <linux/memblock.h>
 #endif
@@ -9855,24 +9854,14 @@ static struct platform_device *surf_devices[] __initdata = {
 #ifdef CONFIG_MSM_MULTIMEDIA_USE_ION
 static struct ion_cp_heap_pdata cp_mm_ion_pdata = {
 	.permission_type = IPT_TYPE_MM_CARVEOUT,
-	.align = SZ_64K,
-	.request_region = request_smi_region,
-	.release_region = release_smi_region,
-	.setup_region = setup_smi_region,
-	.iommu_map_all = 1,
-	.iommu_2x_map_domain = VIDEO_DOMAIN,
-};
-
-static struct ion_cp_heap_pdata cp_mfc_ion_pdata = {
-	.permission_type = IPT_TYPE_MFC_SHAREDMEM,
 	.align = PAGE_SIZE,
 	.request_region = request_smi_region,
 	.release_region = release_smi_region,
 	.setup_region = setup_smi_region,
 };
 
-static struct ion_cp_heap_pdata cp_wb_ion_pdata = {
-	.permission_type = IPT_TYPE_MDP_WRITEBACK,
+static struct ion_cp_heap_pdata cp_mfc_ion_pdata = {
+	.permission_type = IPT_TYPE_MFC_SHAREDMEM,
 	.align = PAGE_SIZE,
 };
 
