8baa15733e5551b6d15cfb715c2f649525f7189d
Emmanuel Utomi <emmanuelutomi@gmail.com>
2014-08-02 09:47:30 -0500

    WildKernel date: use smaller year format

--- flashable/tools/updatewild.sh
+++ flashable/tools/updatewild.sh
@@ -4,7 +4,7 @@ latestversion=008
 # date "+%y%m%d"
 latestdate=140802
 latestDL="http://forum.xda-developers.com/showthread.php?t=2765196"
-#date "+%A, %B %d %Y"
+#date "+%A, %B %d %y"
 latestdateliteral="Saturday, August 02 2014"
 
 input="$1"
