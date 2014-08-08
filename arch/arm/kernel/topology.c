fb833aa75540139617b644969466b69e8dc9e717
Chet Kener <Cl3Kener@gmail.com>
2014-08-08 01:28:12 -0400

    topology: Enable Multicore Powersavings to 2
    Signed-off-by: Chet Kener <Cl3Kener@gmail.com>

--- arch/arm/kernel/topology.c
+++ arch/arm/kernel/topology.c
@@ -99,7 +99,7 @@ struct cputopo_arm cpu_topology[NR_CPUS];
  * cpu topology mask update management
  */
 
-static unsigned int prev_sched_mc_power_savings = 0;
+static unsigned int prev_sched_mc_power_savings = 2;
 static unsigned int prev_sched_smt_power_savings = 0;
 
 ATOMIC_NOTIFIER_HEAD(topology_update_notifier_list);
