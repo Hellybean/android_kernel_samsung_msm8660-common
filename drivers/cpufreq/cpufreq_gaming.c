dae35e5afecf4413717d8f9ac42d00c54081bcb6
Chet Kener <Cl3Kener@gmail.com>
2014-08-08 15:53:11 -0400

    Misc cpufreq tweaks
    * I like them to look uniform
    
    Signed-off-by: Chet Kener <Cl3Kener@gmail.com>

--- drivers/cpufreq/cpufreq_gaming.c
+++ drivers/cpufreq/cpufreq_gaming.c
@@ -135,7 +135,7 @@ static int cpufreq_governor_gaming(struct cpufreq_policy *policy,
 static
 #endif
 struct cpufreq_governor cpufreq_gov_gaming = {
-	.name = "gaming",
+	.name = "Gaming",
 	.governor = cpufreq_governor_gaming,
 	.max_transition_latency = 10000000,
 	.owner = THIS_MODULE,
@@ -712,7 +712,7 @@ static struct attribute *interactive_attributes[] = {
 
 static struct attribute_group interactive_attr_group = {
 	.attrs = interactive_attributes,
-	.name = "gaming",
+	.name = "Gaming",
 };
 
 static int cpufreq_governor_gaming(struct cpufreq_policy *policy,
