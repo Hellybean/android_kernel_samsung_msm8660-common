cbaaf1e1d7f3239af72ac3813e505767f518be30
Chet Kener <Cl3Kener@gmail.com>
2014-08-08 00:36:13 -0400

    Crack Smoker!
    Signed-off-by: Chet Kener <Cl3Kener@gmail.com>

--- arch/arm/mach-msm/msm_mpdecision.c
+++ arch/arm/mach-msm/msm_mpdecision.c
@@ -48,7 +48,7 @@
 #define MSM_MPDEC_STARTDELAY            70000
 #define MSM_MPDEC_DELAY                 130
 #define MSM_MPDEC_PAUSE                 10000
-#define MSM_MPDEC_IDLE_FREQ             96000
+#define MSM_MPDEC_IDLE_FREQ             384000
 #ifdef CONFIG_MSM_MPDEC_INPUTBOOST_CPUMIN
 #define MSM_MPDEC_BOOSTTIME             1000
 #define MSM_MPDEC_BOOSTFREQ_CPU0        918000
