40694d113acd6577c4144551225a065efe88db69
Emmanuel Utomi <emmanuelutomi@gmail.com>
2014-10-12 00:20:26 -0500

    cpu_boost: sultanxda hates children
    * Probably because he is "ghey"
    
    Signed-off-by: Chet Kener <Cl3Kener@gmail.com>

--- include/linux/cpu_boost.h
+++ include/linux/cpu_boost.h
@@ -41,8 +41,8 @@ void cpu_boost_shutdown(void);
 void cpu_boost_startup(void);
 
 #else
-static inline void cpu_boost_shutdown(void);
-static inline void cpu_boost_startup(void);
+static inline void cpu_boost_shutdown(void) { }
+static inline void cpu_boost_startup(void) { }
 static inline void cpu_boost_timeout(unsigned int freq_mhz, unsigned int duration_ms) { }
 static inline void cpu_boost(unsigned int freq_mhz) { }
 static inline void cpu_unboost(void) { }
